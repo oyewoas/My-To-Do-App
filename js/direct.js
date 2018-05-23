b05af7967dfa3a2658b17b5628ca755805c0be47
OYEWO Ayooluwa <31412106+oyewoas@users.noreply.github.com>
2018-05-23 10:45:07 +0100

    Add files via upload

---
+++ js/direct.js
@@ -0,0 +1,9 @@
+//user click the add button
+//add text in the text field to todo list
+document.getElementById('add').addEventListener('click', function() {
+    var value = document.getElementById('item').value;
+    if (value) {
+
+    }
+
+});
